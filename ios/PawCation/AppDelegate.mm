525054d5d98819b14d1934e5e87aed7e7f9410ed
Hamza Hasan <shamzahasan88@gmail.com>
2026-02-11 04:48:21 +0500

    Refactor code structure for improved readability and maintainability

--- ios/PawCation/AppDelegate.mm
+++ ios/PawCation/AppDelegate.mm
@@ -1,16 +1,18 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <Firebase/Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+  
   self.moduleName = @"PawCation";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
